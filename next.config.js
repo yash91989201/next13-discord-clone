b037d338da89dab4e4a94ffdcbbfcfcac414187f
yash91989201 <yashraj6370@gmail.com>
2023-08-30 22:50:47 +0530

    update next config for getting final build file

--- next.config.js
+++ next.config.js
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   webpack: (config) => {
     config.externals.push({
       "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil"
+      bufferutil: "commonjs bufferutil",
     });
 
     return config;
   },
   images: {
-    domains: [
-      "uploadthing.com"
-    ]
-  }
-}
+    domains: ["uploadthing.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
